99ae8fe1e89f59155bf747e6b21d4321efac6ca0
brentschneider <brent@milkshakeinteractive.com>
2016-05-25 15:17:42 -0700

    my weather 2.01 - Added Semantic-ui framework

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# web-3-starter
-Starter Files for Web 3
+# Web 3 - Weather app
+
+[Basecamp Card](https://3.basecamp.com/3319859/buckets/622388/documents/125108129)
+
+Tools:
+- [semantic-ui](http://semantic-ui.com/)
+- [simpleweatherjs](Refer: http://simpleweatherjs.com)
+- [Manicoth icons](http://codepen.io/manikoth/pen/rxXrvb.css)
--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,60 @@
 // Third, load any plugins (optional)
 // @codekit-prepend "plugins.js";
 
+$('.menu .item')
+  .tab()
+;
+
 // My Scripts
 $(document).ready(function() {
+
+
+
+  $.simpleWeather({
+    location: 'Spokane, WA',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+
+      $("#spokane .city").html(weather.city);
+			$("#spokane .temp").html(weather.temp);
+			// $(" .cond-code").html(weather.currently);
+      $("#spokane i").addClass( 'icon-' + weather.code );
+
+
+      if (weather.temp < 50 ) {
+          $("body").addClass("cold");
+      }else if (weather.temp > 50 ) {
+          $("body").addClass("hot");
+      } else {
+        $("body").addClass("black");
+      }
+
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+
+
+
+  });
   $.simpleWeather({
     location: 'Cheney, WA',
     woeid: '',
     unit: 'f',
     success: function(weather) {
 
-      $(".city").html(weather.city);
-			$(".temp").html(weather.temp);
-      $('i').addClass( 'icon-' + weather.code );
-
+      $("#cheney .city").html(weather.city);
+			$("#cheney .temp").html(weather.temp);
+			// $(" .cond-code").html(weather.currently);
+      $("#cheney i").addClass( 'icon-' + weather.code );
     },
+
     error: function(error) {
-      console().log( error );
       $("#weather").html('<p>'+error+'</p>');
     }
   });
+
+
 
 });
